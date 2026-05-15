eeb03065ebfcd10e2120806d9bed033dfe6400b2
tompsen99 <tompsen99@users.noreply.github.com>
2026-05-15 22:04:51 +0800

    enable GET and POST for analyze api

--- functions/api/analyze.js
+++ functions/api/analyze.js
@@ -1,13 +1,32 @@
-export async function onRequestPost() {
-  return new Response(
-    JSON.stringify({
-      success: true,
-      result: "API 已连通"
-    }),
-    {
-      headers: {
-        "Content-Type": "application/json"
+export async function onRequest({ request }) {
+
+  if (request.method === "GET") {
+    return new Response(
+      JSON.stringify({
+        success: true,
+        message: "Cloudflare Pages Functions 已连通（GET）"
+      }),
+      {
+        headers: {
+          "Content-Type": "application/json"
+        }
       }
-    }
-  );
+    );
+  }
+
+  if (request.method === "POST") {
+    return new Response(
+      JSON.stringify({
+        success: true,
+        message: "Cloudflare Pages Functions 已连通（POST）"
+      }),
+      {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+    );
+  }
+
+  return new Response("Method Not Allowed", { status: 405 });
 }
